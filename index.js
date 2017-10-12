9aa2ef7a6a37a6998cca4c472af3af8fff7a0075
Ayumi Saito <ayumisaito27@gmail.com>
2017-10-12 11:49:02 +0200

    added basic html and jquery

---
+++ index.js
@@ -0,0 +1,42 @@
+// Show scroll up bar
+  $(window).scroll(function() {
+    if ($(this).scrollTop()< 100)
+     {
+        $('.scroll').fadeOut();
+     }
+    else
+     {
+      $('.scroll').fadeIn();
+     }
+ });
+
+// Scroll up to top
+$(".scroll").click(function(){
+    $("html, body").animate({ scrollTop: 0}, "slow");
+});
+
+// load post
+$(".loadPost").click(function(){
+    $("#postFifth").css({ 'display' : '' });
+    $(this).html("End");
+    console.log("click")
+})
+
+// load portfolio
+$(".loadPortfolio").click(function(){
+    $("#portfolioFourth").css({ 'display' : '' });
+    $(this).html("End");
+    console.log("click")
+})
+
+// show nav bar {
+$("#navBar .navShow").click(function(){
+    if( $("#navBar .nav").hasClass( "show" )) {
+         $("#navBar .nav").css({ 'display' : '' });
+         $("#navBar .nav").removeClass("show");
+    } else {
+         $("#navBar .nav").css({ 'display' : 'none' });
+         $("#navBar .nav").addClass("show");
+    }
+     console.log("click")
+})
